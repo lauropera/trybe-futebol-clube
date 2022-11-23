836331fb36bc64112910bb81c965a4fa9401f379
Lauro Pereira <lauropera01@gmail.com>
2022-11-23 19:18:15 -0300

    Corrigindo interfaces de User

--- app/backend/src/interfaces/IUserController.ts
+++ app/backend/src/interfaces/IUserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 
 export default interface IUserController {
-  login(req: Request, res: Response): void;
+  login(req: Request, res: Response): Promise<void>;
+  getUserRole(req: Request, res: Response): Promise<void>;
 }
