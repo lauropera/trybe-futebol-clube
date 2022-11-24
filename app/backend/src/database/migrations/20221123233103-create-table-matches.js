aee47094470ba90e83c677c5cae7d032fa914712
Lauro Pereira <lauropera01@gmail.com>
2022-11-23 21:39:45 -0300

    Criando a tabela "Matches" e seu model

---
+++ app/backend/src/database/migrations/20221123233103-create-table-matches.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("matches", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      home_team_goals: {
+        allowNull: null,
+        type: Sequelize.INTEGER,
+      },
+      away_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      away_team_goals: {
+        allowNull: null,
+        type: Sequelize.INTEGER,
+      },
+      in_progress: {
+        allowNull: null,
+        type: Sequelize.BOOLEAN,
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches');
+  }
+};
---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,39 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import Team from './Team';
+
+class Match extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: INTEGER,
+  homeTeamGoals: INTEGER,
+  awayTeam: INTEGER,
+  awayTeamGoals: INTEGER,
+  inProgress: BOOLEAN,
+}, {
+  sequelize: db,
+  modelName: 'match',
+  tableName: 'matches',
+  underscored: true,
+  timestamps: false,
+});
+
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'matches' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'matches' });
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'team' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'team' });
+
+export default Match;
